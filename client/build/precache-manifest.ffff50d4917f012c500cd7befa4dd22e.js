cd81ccdeae85c76cb98ed4526b76a6d06f375ffc
bepul0 <bepul.cse.ru@gmail.com>
2020-08-08 08:23:37 +0600

    build folder added

---
+++ client/build/precache-manifest.ffff50d4917f012c500cd7befa4dd22e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e0e824d526583ca420d6d55e1a6c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e6cd9d5f2dba2c6cc49",
+    "url": "/static/css/main.83648ea1.chunk.css"
+  },
+  {
+    "revision": "c197f6111def9dc08c70",
+    "url": "/static/js/2.ad571d88.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.ad571d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6cd9d5f2dba2c6cc49",
+    "url": "/static/js/main.69fa9513.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
